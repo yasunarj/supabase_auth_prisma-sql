5fe6c5d6f975b59b80a57f2a7ec67f9481b06cb7
nari <yasunarj@gmail.com>
2025-06-14 18:00:23 +0900

    fix: github/libのステップの順番の変更

--- app/github/lib/steps.ts
+++ app/github/lib/steps.ts
@@ -1,5 +1,5 @@
 export const githubLearnSteps = [
-  { title: "ステップ１", content: "featureブランチを作成" },
-  { title: "ステップ２", content: "Pull Requestを作成してmergeを実行する" },
-  { title: "ステップ３", content: "Issueを作成してpull requestに繋げる" },
+  { title: "ステップ１", content: "Issueを作成してpull requestに繋げる" },
+  { title: "ステップ２", content: "featureブランチを作成" },
+  { title: "ステップ３", content: "Pull Requestを作成してmergeを実行する" },
 ];
